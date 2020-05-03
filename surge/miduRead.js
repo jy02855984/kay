ac352e1eee1c280dc932348855c29ac83e9f3bda
jy02855984 <61726902+jy02855984@users.noreply.github.com>
2020-05-03 21:51:34 +0800

    Update miduRead.js

--- surge/miduRead.js
+++ surge/miduRead.js
@@ -18,7 +18,7 @@ const DeleteCookieAll = false; // 清除所有
 const DeleteCookieOne = false; // 清除账户一
 const DeleteCookieTwo = false; // 清除账户二
 
-const debug = false; // 开启debug模式,每次脚本执行会显示通知,默认false
+const debug = true; // 开启debug模式,每次脚本执行会显示通知,默认false
 
 const bind = true; // 绑定作者邀请码,默认true,可更改为false
 
